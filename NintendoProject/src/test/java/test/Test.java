73acd273574d9297d76e275687e128a4276fb829
Arthur Diot <arthur.diot@gmail.com>
2023-01-11 09:53:09 +0100

    init objets

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -1,10 +1,18 @@
 package test;
 
+import model.Console;
+import model.Jeu;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Console c = new Console("Nintendo Gamecube");
+		Jeu j1 = new Jeu("Super Mario Sunshine",c);
+		Jeu j2 = new Jeu("Super Smash Bros Melee", c);
+		Jeu j3 = new Jeu("Tales of Symphonia", c);
+		Jeu j4 = new Jeu("The Legend of Zelda : Wind Waker",c);
+		Jeu j5 = new Jeu("Resident Evil 4", c);
+		
 	}
 
 }
